94ec9c5dc74db3414f64b20ce111445858b1bcba
KAKAN <22027824+theKAKAN@users.noreply.github.com>
2023-07-27 17:35:09 +0530

    LAB1 complete

---
+++ 23-07-27_LAB1/q4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void EXCHANGE( int *p1, int *p2 ){
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+
+void ROTATE_RIGHT( int ar[], int n ){
+    n = n-1;
+    // 5 as input should mean the 5th element, not 6th( counted from 0 )
+    int last = ar[n];
+    for( int i = n; i > 0; i-- ){
+        EXCHANGE( &ar[i], &ar[i-1] );
+    }
+    EXCHANGE( &ar[0], &last );
+}
+
+int main(){
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d integers now: ", n);
+    for( int i = 0; i < n; i++ )
+        scanf("%d", &arr[i]);
+
+    
+    printf("\nOriginal Array: ");
+    for( int i = 0; i < n; i++ )
+        printf("%d ", arr[i]);
+    
+    ROTATE_RIGHT( arr, 5 );
+
+    printf("\nModified Array: ");
+    for( int i = 0; i < n; i++ )
+        printf("%d ", arr[i]);
+    
+    printf("\n");
+    return 0;
+}
