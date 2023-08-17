303681944005067ba35fd63c7d7b44a129a13988
KAKAN <22027824+theKAKAN@users.noreply.github.com>
2023-08-17 16:42:07 +0530

    L4Q1: Complete

--- 23-08-17_LAB4/q1.c
+++ 23-08-17_LAB4/q1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
+#define SEC_TO_NS 1000000000
+#define MAX_READ 400
+
 void merge(int elements[], int low, int middle, int high, int* comparisions) {
     int n1 = middle - low + 1;
     int n2 = high - middle;
@@ -41,11 +45,9 @@ void mergeSort(int elements[], int low, int high, int* comparisions) {
 }
 
 int main() {
-    int choice;
     FILE *inputFile, *outputFile;
-    int elements[500]; 
-    int size;
-    int comparisons = 0;
+    int elements[MAX_READ]; 
+    int choice, size, comparisons = 0;
     clock_t start, end;
     double cpu_time_used;
 
@@ -65,18 +67,18 @@ int main() {
         switch (choice) {
             case 1:
                 inputFile = fopen("q1_inAsc.dat", "r");
-                outputFile = fopen("outMergeAsce.txt", "w");
+                outputFile = fopen("q1_outAsc.dat", "w");
                 break;
             case 2:
-                inputFile = fopen("inDescending.txt", "r");
-                outputFile = fopen("outMergeDesc.txt", "w");
+                inputFile = fopen("q1_inDesc.dat", "r");
+                outputFile = fopen("q1_outDesc.dat", "w");
                 break;
             case 3:
-                inputFile = fopen("inRandom.txt", "r");
-                outputFile = fopen("outMergeRand.txt", "w");
+                inputFile = fopen("q1_inRand.dat", "r");
+                outputFile = fopen("q1_outRand.dat", "w");
                 break;
             default:
-                printf("invalid choice entered\n");
+                printf("Invalid choice entered\n");
                 continue;
         }
 
@@ -85,27 +87,34 @@ int main() {
             return 1;
         }
 
+
+        // Print the file
+        printf("Before sorting: ");
         size = 0;
-        while (fscanf(inputFile, "%d", &elements[size]) == 1) {
+        while( size < MAX_READ && fscanf(inputFile, "%d", &elements[size]) == 1 ) {
+            // Print the element
+            printf("%d ", elements[size]);
+            // Increase size after printing
             size++;
         }
-
         fclose(inputFile);
 
         comparisons = 0;
         start = clock();
         mergeSort(elements, 0, size - 1, &comparisons);
         end = clock();
-        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        cpu_time_used = ((double) (end - start))*SEC_TO_NS / CLOCKS_PER_SEC;
 
+        // Print to output too
+        printf("\nAfter sorting: ");
         for (int i = 0; i < size; i++) {
             fprintf(outputFile, "%d ", elements[i]);
+            printf("%d ", elements[i]);
         }
         fclose(outputFile);
 
-        printf("Sorting completed.\n");
-        printf("Number of comparisons: %d\n", comparisons);
-        printf("Execution time: %f seconds\n", cpu_time_used);
+        printf("\nNumber of comparisons: %d\n", comparisons);
+        printf("Execution time: %.2f nanoseconds\n", cpu_time_used);
     }
 
     return 0;
